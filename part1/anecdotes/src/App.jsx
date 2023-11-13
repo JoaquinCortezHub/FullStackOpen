f1e3b0fcb907024ac8a9b079975762a471df7b06
Joaquín Cortez <joaquinlucascortez@gmail.com>
2023-11-13 20:50:32 -0300

    Exercises 1.13 & 1.14

--- part1/anecdotes/src/App.jsx
+++ part1/anecdotes/src/App.jsx
@@ -10,21 +10,46 @@ const App = () => {
     'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
     'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
     'The only way to go fast, is to go well.'
-  ]
+  ];
   
   const [selected, setSelected] = useState(0);
+  const [votes, setVotes] = useState(Array(anecdotes.length).fill(0));
+
   const randomAnecdote = () => {
-    const anecdoteIndex = Math.floor(Math.random() * anecdotes.length);
+    const anecdoteIndex = Math.floor(Math.random() * anecdotes.length); //? <-- selects random anecdote
     setSelected(anecdoteIndex);
-  }
+  };
+
+  const anecdoteVote = () => {
+    const newVote = [...votes];
+    newVote[selected] += 1;
+    setVotes(newVote);
+  };
+
+  const mostVotedAnecdote = votes.indexOf(Math.max(...votes));
 
   return (
     <div>
-      <div>{anecdotes[selected]}</div>
+      <div>
+        <h2>Anecdote of the day</h2>
+        {anecdotes[selected]}
+        <br />
+        Votes: {votes[selected]}
+      </div>
       <br />
       <button onClick={randomAnecdote}>Random Anecdote</button>
+      <button onClick={anecdoteVote}>Vote</button>
+
+      {Math.max(...votes) > 0 && (
+        <div>
+          <h2>Most voted anecdote</h2>
+          {anecdotes[mostVotedAnecdote]}
+          <br />
+          Votes: {votes[mostVotedAnecdote]}
+        </div>
+      )}
     </div>
-  )
-}
+  );
+};
 
 export default App
