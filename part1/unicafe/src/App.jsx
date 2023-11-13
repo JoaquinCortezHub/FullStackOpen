4df6544e59bc504b55e03a23187111476da59b58
Joaquín Cortez <joaquinlucascortez@gmail.com>
2023-11-13 17:15:53 -0300

    Exercise 1.7

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,10 +1,11 @@
 import { useState } from 'react'
 
 const App = () => {
-  // save clicks of each button to its own state
+  // save clicks of each button to its own state (destructuring)
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const all = good + neutral+ bad
 
   return (
     <div>
@@ -16,7 +17,10 @@ const App = () => {
       <h2>Statistics</h2>
       good: {good}<br />
       neutral: {neutral}<br />
-      bad: {bad}
+      bad: {bad}<br />
+      all: {all}<br />
+      average: {(good * 1 + neutral * 0 + bad * -1) / all}<br />
+      positive percent: {((good * 100) / all).toFixed(2)}%<br/>
     </div>
   )
 }
