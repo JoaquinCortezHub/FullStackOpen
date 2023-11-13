c67ce3c960b58475521f0f2f911c6eaf7eac993f
Joaquín Cortez <joaquinlucascortez@gmail.com>
2023-11-13 18:46:42 -0300

    Exercise 1.8

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,11 +1,28 @@
 import { useState } from 'react'
 
+const Statistics = ({good, neutral, bad, all}) => {  //* <--  Remember to use {} when passing specific name props
+  const average = (good * 1 + neutral * 0 + bad * -1) / all;
+  const positivePercentage = ((good * 100) / all).toFixed(2); //? <-- toFixed(x) shows the amount of numbers after decimal
+
+  return (
+    <div>
+      <h1>Statistics</h1>
+      good: {good}<br />
+      neutral: {neutral}<br />
+      bad: {bad}<br />
+      all: {all}<br />
+      average: {average}<br />
+      positive percentage: {positivePercentage}%<br />
+    </div>
+  )
+}
+
 const App = () => {
   // save clicks of each button to its own state (destructuring)
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  const all = good + neutral+ bad
+  const all = good + neutral + bad
 
   return (
     <div>
@@ -14,13 +31,7 @@ const App = () => {
       <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
       <button onClick={() => setBad(bad + 1)}>bad</button>
       <br />
-      <h2>Statistics</h2>
-      good: {good}<br />
-      neutral: {neutral}<br />
-      bad: {bad}<br />
-      all: {all}<br />
-      average: {(good * 1 + neutral * 0 + bad * -1) / all}<br />
-      positive percent: {((good * 100) / all).toFixed(2)}%<br/>
+      <Statistics good={good} neutral={neutral} bad={bad} all={all} />
     </div>
   )
 }
