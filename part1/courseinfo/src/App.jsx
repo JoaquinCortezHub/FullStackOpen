b1435d459fb9e91c3213829a1feb1337c12e3631
Joaquín Cortez <joaquinlucascortez@gmail.com>
2023-11-14 17:28:27 -0300

    Exercise 2.4

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -2,7 +2,7 @@
 // Consts are still in APP but passed by props to other components.
 
 const Header = ({courseName}) => {
-  return <h1>{courseName}</h1>;
+  return <h2>{courseName}</h2>;
 };
 
 const Part = ({part}) => {
@@ -43,29 +43,60 @@ const Total = ({parts}) => {
 };
 
 const App = () => {
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      }
-    ]
-  }
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
 
-  return <Course course={course} />
+  return (
+    <div>
+      <h1>Web Development Curriculum</h1>
+      {courses.map((course) => (
+      <Course key={course.id} course={course} />
+    ))}
+    </div>
+
+  )
 }
 
 
